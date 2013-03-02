e1b1e01c5571eab5568d8f91f27c6aeb7666fe29
MFB <bertoa@lcc.uma.es>
2013-03-02 10:09:30 +0100

    GitAux modificado

--- GitAux.java
+++ GitAux.java
@@ -5,8 +5,7 @@
 package prgitejemplo;
 
 /**
- *
- * @author bertoa
+ * @author MFB
  */
 public class GitAux {
     
@@ -19,6 +18,7 @@ public class GitAux {
     }
     
     public void metodoComunitario(){
+    	// Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
     }
 }
