3e39875e7e84a153e0dae1d11613095e4f005590
MFB <bertoa@lcc.uma.es>
2013-03-02 12:38:03 +0100

    testing en GitAux

--- GitAux.java
+++ GitAux.java
@@ -21,4 +21,8 @@ public class GitAux {
     	// Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
     }
+    public void testGitAux(){
+    	// metodo rama testing
+	System.out.println("Metodo de testing");
+    }
 }
