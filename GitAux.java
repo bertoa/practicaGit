9f12134c052af93ff4059fdbdd75f6fb3aadea35
MFB <bertoa@lcc.uma.es>
2013-03-02 15:57:28 +0100

    modifica alumno DOS

--- GitAux.java
+++ GitAux.java
@@ -15,11 +15,13 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
-    }
+	System.out.println("insertado por alumno2"); // modif alumno 2
+}
     
     public void metodoComunitario(){
     	// Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
+	System.out.println("alumno 2"); // insertado por alumno 2
     }
     public void testGitAux(){
     	// metodo rama testing
